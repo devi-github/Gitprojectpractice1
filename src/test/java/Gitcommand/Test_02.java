e4e2233b7bcd69fb03521c866f75977a9c95d533
devi-github <devireddyporeddy@gmail.com>
2023-09-06 15:34:40 +0530

    changes files

--- src/test/java/Gitcommand/Test_02.java
+++ src/test/java/Gitcommand/Test_02.java
@@ -4,9 +4,9 @@ public class Test_02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    String str="devireddyporeddy";
+    String str="deviredy";
     String str1="poreddy";
-    System.out.println(str1);
+    System.out.println(str);
 	}
 
 }
