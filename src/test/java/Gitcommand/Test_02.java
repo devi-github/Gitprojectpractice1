4c8be0cd6a0c996532ca8216cfe11e67bfac7fd4
devi-github <141806869+devi-github@users.noreply.github.com>
2023-09-06 15:01:44 +0530

    Update Test_02.java

--- src/test/java/Gitcommand/Test_02.java
+++ src/test/java/Gitcommand/Test_02.java
@@ -4,7 +4,7 @@ public class Test_02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    String str="devireddy";
+    String str="devireddyporeddy";
     String str1="poreddy";
     System.out.println(str1);
 	}
